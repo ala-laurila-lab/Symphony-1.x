aff07769a01ad7a2269c5f63d9217a589dcb4015
Sathish Kumar Narayanan <sathish.narayanan@aalto.fi>
2015-11-08 10:21:36 +0200

    rig config for 1tc18

--- Symphony/Rig Configurations/list/Multiclamp.m
+++ Symphony/Rig Configurations/list/Multiclamp.m
@@ -8,31 +8,29 @@ classdef Multiclamp < LabRigConfiguration
     methods   
         %% Note: The names of the devices are important, the can not be changed!
         function createDevices(obj)
-             obj.addMultiClampDevice('Amplifier_01_Ch1', 1, 1, 'ANALOG_OUT.0', 'ANALOG_IN.0');
-             obj.addMultiClampDevice('Amplifier_01_Ch2', 1, 2, 'ANALOG_OUT.1', 'ANALOG_IN.1'); 
-             obj.addMultiClampDevice('Amplifier_02_Ch1', 2, 1, 'ANALOG_OUT.2', 'ANALOG_IN.2');   
-             obj.addMultiClampDevice('Amplifier_02_Ch2', 2, 2, 'ANALOG_OUT.3', 'ANALOG_IN.3'); 
+             obj.addMultiClampDevice('Amplifier_Ch1', 1, 'ANALOG_OUT.0', 'ANALOG_IN.0');
+             obj.addMultiClampDevice('Amplifier_Ch2', 2, 'ANALOG_OUT.1', 'ANALOG_IN.1'); % If adding a 3rd LED Channel, this line needs to be commented out 
+
             %% LED Devices
-            obj.addDevice('Ch1', 'ANALOG_OUT.4', '');
-            obj.addDevice('Ch2', 'ANALOG_OUT.5', '');
+            obj.addDevice('Ch1', 'ANALOG_OUT.2', '');
+            obj.addDevice('Ch2', 'ANALOG_OUT.3', '');
 
             %% Adding the heat controller
-            obj.addDevice('HeatController', '', 'ANALOG_IN.4');
+            obj.addDevice('HeatController', '', 'ANALOG_IN.2');
 
             %% Adding the Optometer
 %             obj.addDevice('Optometer', '', 'ANALOG_IN.3'); %Uncomment if you are using the optometer, make sure the correcct channel is being used
             
-            %% Adding the Rig Switches 
-
-               obj.addDevice('Rig_Switches_0','', 'DIGITAL_IN.0');  % input only
-               obj.addDevice('Rig_Switches_1','', 'DIGITAL_IN.1');  % input only
-               obj.addDevice('Rig_Switches_2','', 'DIGITAL_IN.2');  % input only
-               obj.addDevice('Rig_Switches_3','', 'DIGITAL_IN.3');  % input only
-               obj.addDevice('Rig_Switches_4','', 'DIGITAL_IN.4');  % input only
-%              obj.addDevice('Rig_Switches_5','', 'DIGITAL_IN.5');  % input only
-%              obj.addDevice('Rig_Switches_6','', 'DIGITAL_IN.6');  % input only
-%              obj.addDevice('Rig_Switches_7','', 'DIGITAL_IN.7');  % input only
-%              obj.addDevice('Rig_Switches_8','', 'DIGITAL_IN.8');  % input only
+            %% Adding the Rig Switches
+            obj.addDevice('Rig_Switches_0','', 'DIGITAL_IN.0');  % input only
+            obj.addDevice('Rig_Switches_1','', 'DIGITAL_IN.1');  % input only
+            obj.addDevice('Rig_Switches_2','', 'DIGITAL_IN.2');  % input only
+            obj.addDevice('Rig_Switches_3','', 'DIGITAL_IN.3');  % input only
+%             obj.addDevice('Rig_Switches_4','', 'DIGITAL_IN.4');  % input only
+%             obj.addDevice('Rig_Switches_5','', 'DIGITAL_IN.5');  % input only
+%             obj.addDevice('Rig_Switches_6','', 'DIGITAL_IN.6');  % input only
+%             obj.addDevice('Rig_Switches_7','', 'DIGITAL_IN.7');  % input only
+%             obj.addDevice('Rig_Switches_8','', 'DIGITAL_IN.8');  % input only
 %             obj.addDevice('Rig_Switches_9','', 'DIGITAL_IN.9');  % input only
 %             obj.addDevice('Rig_Switches_10','', 'DIGITAL_IN.10');  % input only
 %             obj.addDevice('Rig_Switches_11','', 'DIGITAL_IN.11');  % input only
